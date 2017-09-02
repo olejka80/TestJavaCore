9ac3ad0da29bfac505f71e28f25b36c465430a55
Oleg Melnichuk <nk.melnichuk@gmail.com>
2017-09-02 14:27:52 +0300

    Create GitHub repository

---
+++ app/src/main/java/com/example/acer/testjavacore/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.acer.testjavacore;
+
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.logging.Logger;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final Logger LOGGER = Logger.getLogger(MainActivity.class.getName());
+
+    public static void main(String[] args) {
+
+        
+    }
+}
